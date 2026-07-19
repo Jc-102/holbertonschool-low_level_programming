08fd33a0c65cba2f032abf07b0ff376bd12e67c1
Jc-102 <jeanc1725@gmail.com>
2026-07-19 18:44:37 -0400

    betty style fix

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,9 +17,9 @@ int main(void)
 	
 	printf("%d ", n);
 	
-	if (n>0)
+	if (n > 0)
 		printf("is positive\n");
-	else if (n==0)
+	else if (n == 0)
 		printf("is zero\n");
 	else
 		printf("is negative\n");

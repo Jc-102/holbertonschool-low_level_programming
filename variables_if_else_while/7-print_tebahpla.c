fce414ee7a6836e28fa11e6e6caf2279699b35d7
Jc-102 <jeanc1725@gmail.com>
2026-07-19 22:19:31 -0400

    reverse alphabet

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -10,8 +10,8 @@ int main (void)
 {
 	char letter;
 
-	for(letter = 'z'; letter >= 'a'; c--)
-		putchar(c);
+	for(letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
 	putchar('\n');
 
 	return (0);

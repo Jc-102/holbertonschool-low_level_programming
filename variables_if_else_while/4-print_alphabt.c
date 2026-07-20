174be1ef00212de40433d181a684304416f6e0b9
Jc-102 <jeanc1725@gmail.com>
2026-07-19 21:47:47 -0400

    fix

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,11 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		if (letter == 'q' || letter == 'e')
 			continue;
 		putchar(letter);
+	}
 	putchat('\n');
 
 	return (0);

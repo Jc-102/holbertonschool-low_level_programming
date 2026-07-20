8deef3b16cfec2043e4c3c9bc513342bee4ea8f7
Jc-102 <jeanc1725@gmail.com>
2026-07-19 22:33:01 -0400

    reverse alphabet

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**\
+ * main - prints numbers of 16 base
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n + '0');
+	for (n = 10; n <= 15; n++)
+		putchar(n - 10 + 'a')
+	putchar('\n');
+
+	return (0);
+}

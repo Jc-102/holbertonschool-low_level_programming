eaf7bc4cc70b40a44fd5013647fc2ae896504aa0
Jc-102 <jeanc1725@gmail.com>
2026-07-26 14:12:29 -0400

    print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to print
+ */
+
+void print_rev(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	length--;
+
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+	_putchar('\n');
+}

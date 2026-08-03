b5f29a59516fc4c47a05b91e7615790c8c745028
Jc07 <jeanc1725@gmail.com>
2026-08-02 23:59:45 -0400

    Update 5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,11 +2,12 @@
 #include <stddef.h>
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * try_sqrt - helper that tests candidate values recursively
  * @n: the number to find the square root of
+ * @i: the candidate square root being tested
  *
- * Return: the natural square root of n, or -1 if n does not 
- * have a natural square root
+ * Return: i if i * i == n, -1 if i * i exceeds n
+ * with no match
  */
 
 int try_sqrt(int n, int i)
@@ -17,6 +18,13 @@ int try_sqrt(int n, int i)
 		return (-1);
 	return (try_sqrt(n, i + 1));
 }
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the natural square root of n, or -1 if none exists
+ */
 
 int _sqrt_recursion(int n)
 {

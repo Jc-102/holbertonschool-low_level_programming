cc8e61219264cd2f35e418496d18e9656bdc95b7
Jc07 <jeanc1725@gmail.com>
2026-08-09 12:58:51 -0400

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - represents a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the name of the dog's owner
+ */
+
 struct dog
 {
 	char *name;
